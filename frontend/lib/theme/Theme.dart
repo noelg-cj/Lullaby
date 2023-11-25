fee653c623007d21c918d14cc93dbfbc2905a358
Noel George C J <noelgeorge2004@gmail.com>
2023-11-25 20:15:15 +0530

    Themes added

---
+++ frontend/lib/theme/Theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: Color(0xffA18CE7),
+  textTheme: TextTheme(
+    headlineLarge: TextStyle().copyWith(color: Color(0xff534C7F))
+  )
+);
