270f790fa23238668ac8537b6c6e3baf10908f8d
Noel George C J <noelgeorge2004@gmail.com>
2023-11-25 20:02:59 +0530

    Home page image added

--- frontend/lib/pages/Home.dart
+++ frontend/lib/pages/Home.dart
@@ -6,9 +6,37 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Image.asset('assets/images/home_bg.png'),
+              Container(
+                alignment: Alignment.center,
+                height: 120,
+                child: Image.asset('assets/images/logo_dark.png')
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                      Colors.transparent,
+                      Colors.white
+                    ])
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      )
     );
   }
 }
