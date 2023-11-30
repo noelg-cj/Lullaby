346cf87f2eb15f644a1023aa9a51468e366b4fb0
Noel George C J <noelgeorge2004@gmail.com>
2023-12-01 00:38:32 +0530

    Blog button parameters

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: lightTheme,
       debugShowCheckedModeBanner: false,
-      home: BlogWidget()
+      home: Home()
     );
   }
 }
---
+++ frontend/lib/pages/blog_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BlogButton extends StatefulWidget {
+  final String title;
+  final int episodes;
+  final String imageurl;
+
+  const BlogButton ({Key? key, required this.title, required this.episodes, required this.imageurl}) : super(key: key);
+
+  @override
+  State<BlogButton> createState() => _BlogButtonState();
+}
+
+class _BlogButtonState extends State<BlogButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const ;
+  }
+}
